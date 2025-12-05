24963203bd8d05870ec6470e645a438efaac96a2
PicoJr <picojr_dev@gmx.com>
2025-12-05 18:21:35 +0100

    Add nice colors and --careful option

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use console::style;
 use dialoguer::Confirm;
 use dialoguer::theme::ColorfulTheme;
 use git2::{Repository, SubmoduleIgnore};
@@ -10,34 +11,67 @@ struct Args {
     /// Make failed checks hard errors
     #[arg(long, default_value = "false")]
     strict: bool,
+    /// Ask confirmation if commit contains a submodule update
+    #[arg(long, default_value = "false")]
+    careful: bool,
 }
 
-fn check_submodules(strict: bool) -> anyhow::Result<()> {
+fn check_submodules(strict: bool, careful: bool) -> anyhow::Result<()> {
     if let Ok(repo) = Repository::open(".") {
         if let Ok(submodules) = repo.submodules() {
-            let mut overlooked_submodules: Vec<String> = vec![];
+            let mut modified_not_staged_submodules: Vec<String> = vec![];
+            let mut modified_staged_submodules: Vec<String> = vec![];
             for submodule in submodules {
                 if let Some(name) = submodule.name() {
                     debug!("checking submodule: {name}");
                     let status = repo.submodule_status(name, SubmoduleIgnore::None)?;
                     if status.is_wd_modified() {
                         debug!("{name} is modified but not staged");
-                        overlooked_submodules.push(String::from(name));
+                        modified_not_staged_submodules.push(String::from(name));
+                    }
+                    if status.is_index_modified() {
+                        debug!("{name} is modified and staged");
+                        modified_staged_submodules.push(String::from(name));
                     }
                 } else {
                     warn!("submodule does not have a name");
                 }
             }
 
-            if !overlooked_submodules.is_empty() {
-                let mut confirmation_message_lines = vec![
-                    "The following submodules are modified but not staged for commit:".to_string(),
-                ];
-                for name in overlooked_submodules {
-                    confirmation_message_lines.push(format!("* `{name}` is modified and not staged, (`git add {name}` to add submodule to staging)"));
+            let mut confirmation_message_lines = vec![];
+            if !modified_not_staged_submodules.is_empty() {
+                confirmation_message_lines.push(format!(
+                    "{} {} {}",
+                    style("The following submodules are").bold(),
+                    style("modified but not staged").bold().red(),
+                    style("for commit:").bold(),
+                ));
+                for name in &modified_not_staged_submodules {
+                    confirmation_message_lines.push(format!(
+                        "* {} (`git add {name}` to add submodule to staging)",
+                        style(name).bold().red(),
+                    ));
                 }
-                confirmation_message_lines.push("Do you wish to continue anyway?".to_string());
+            }
+            if !modified_staged_submodules.is_empty() {
+                confirmation_message_lines.push(format!(
+                    "{} {} {}",
+                    style("The following submodules are").bold(),
+                    style("modified and staged").bold().green(),
+                    style("for commit:").bold(),
+                ));
+                for name in &modified_staged_submodules {
+                    confirmation_message_lines.push(format!(
+                        "* `{}` (`git restore --staged {name}` to remove submodule from staging)",
+                        style(name).bold().green(),
+                    ));
+                }
+            }
+            confirmation_message_lines.push("Do you wish to continue anyway?".to_string());
+            let ask_confirmation = !modified_not_staged_submodules.is_empty()
+                || (!modified_staged_submodules.is_empty() && careful);
 
+            if ask_confirmation {
                 let confirmation = Confirm::with_theme(&ColorfulTheme::default())
                     .with_prompt(confirmation_message_lines.join("\n"))
                     .default(false)
@@ -48,7 +82,6 @@ fn check_submodules(strict: bool) -> anyhow::Result<()> {
                     anyhow::bail!("Commit aborted by user.")
                 }
             }
-
         } else {
             error!("failed to list submodules");
             if strict {
@@ -67,5 +100,5 @@ fn check_submodules(strict: bool) -> anyhow::Result<()> {
 fn main() -> anyhow::Result<()> {
     env_logger::init();
     let args = Args::parse();
-    check_submodules(args.strict)
+    check_submodules(args.strict, args.careful)
 }
